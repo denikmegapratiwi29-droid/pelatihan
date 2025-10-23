8d68cff1cdcd729c53c14c4b9b4122ad13935e6c
denikmegapratiwi29-droid <denikmegapratiwi29@gmail.com>
2025-10-23 10:14:42 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. pilih tombol berdasarkan ID-nya
+const tombolSapa = document.getElementById('sapaButton');
+// 2.tambahan 'event listener'untuk aksi 'clik'
+tombolsapa,addEventListener('clik',function() {
+// 3. jalankan kode ini ketika tombol di-klik
+alert('halo! terima kasih!');
+)):
